42fc0dd911069b567139bede5e9e758c9aea170f
kyj0503 <heroria0503@gmail.com>
2024-03-31 03:12:12 +0900

    인텔리제이에서 자바11 사용한 백준 문제 풀이

--- Class1/src/No1330.java
+++ Class1/src/No1330.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
+
 public class No1330 {
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int A = in.nextInt();
-        int B = in.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
-        in.close();
+        sc.close();
 
         if(A>B) System.out.println(">");
         else if(A<B) System.out.println("<");
         else System.out.println("==");
 
     }
-
 }
